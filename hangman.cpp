fee82ba652be620283fc16ed9ba2ae27fc2b42d9
Ahmedm2002 <ahmedmujtaba0129@gmail.com>
2024-07-12 07:28:16 +0500

    Test Commit

--- hangman.cpp
+++ hangman.cpp
@@ -14,6 +14,7 @@ void displayhangman(string fName){
 		cout << " |_______   " << endl;
 		cout << endl;
 }
+// this is a test commit 
 void resetGame(vector<string> &usedWords, string & sectretLetter)
 {
     usedWords.clear();
